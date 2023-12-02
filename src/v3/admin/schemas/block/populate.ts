5dd8b13f876ee0905e07d9f2d1f2b36aff11f3dc
Kien Trung <trungle30902@gmail.com>
2023-12-03 01:52:53 +0700

    Fix block populate schema space nullable

--- src/v3/admin/schemas/block/populate.ts
+++ src/v3/admin/schemas/block/populate.ts
@@ -3,9 +3,11 @@ import { blockSchema } from "./base";
 import { spaceSchema } from "../space/base";
 
 const blockPopulateSchema = blockSchema.omit({ space_id: true }).and(
-  z.object({
-    space: spaceSchema,
-  }),
+  z
+    .object({
+      space: spaceSchema,
+    })
+    .nullable(),
 );
 
 type TBlockPopulateSchema = z.infer<typeof blockPopulateSchema>;
