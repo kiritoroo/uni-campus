63f2332f88b079ec341a1b3751e452704a3af0f2
Kien Trung <trungle30902@gmail.com>
2023-12-11 03:16:36 +0700

    Update theme audio

--- src/v3/site/scenes/campus/Entry.tsx
+++ src/v3/site/scenes/campus/Entry.tsx
@@ -2,8 +2,12 @@ import { useBuildingServices } from "@v3/site/hooks/useBuildingServices";
 import GLCanvas from "./webgl/GLCanvas";
 import { useCampusSceneStore } from "./hooks/useCampuseSceneStore";
 import { memo, useEffect } from "react";
+import ThemeAudio from "./components/ThemeAudio";
+import { useGlobalStore } from "@v3/site/hooks/useGlobalStore";
 
 const Entry = memo(() => {
+  const globalStore = useGlobalStore();
+
   const campusSceneStore = useCampusSceneStore();
   const campusSceneActions = campusSceneStore.use.actions();
 
@@ -23,8 +27,16 @@ const Entry = memo(() => {
   }
 
   return (
-    <div className="relative h-screen w-screen bg-[#e9e9e9]">
+    <div
+      className="relative h-screen w-screen bg-[#e9e9e9]"
+      onClick={() => {
+        setTimeout(() => {
+          globalStore.setState({ isInteractive: true });
+        }, 5000);
+      }}
+    >
       <GLCanvas />
+      <ThemeAudio />
     </div>
   );
 });
