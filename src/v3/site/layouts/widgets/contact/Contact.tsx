00869670977eb388aea1666e1f14b2e9ef1b4748
Kien Trung <trungle30902@gmail.com>
2023-12-17 02:48:00 +0700

    Update contacts widget

--- src/v3/site/layouts/widgets/contact/Contact.tsx
+++ src/v3/site/layouts/widgets/contact/Contact.tsx
@@ -1,7 +1,7 @@
 import { useGlobalStore } from "@v3/site/hooks/useGlobalStore";
-import { X } from "lucide-react";
+import { GithubIcon, GlobeIcon, MailIcon, X } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const Contact = () => {
@@ -42,6 +42,47 @@ const Contact = () => {
         <div className="relative bg-white/60 px-8 py-5 backdrop-blur-[5px] md:px-32 md:py-20">
           <div className="py-5 pb-8 text-center font-geist text-3xl font-semibold uppercase">
             Contact
+          </div>
+
+          <div className="flex flex-col items-start justify-center space-y-3 py-5">
+            <div className="flex items-center justify-start gap-x-5">
+              <div className="flex items-stretch justify-center">
+                <div className="flex items-center justify-center bg-white px-4 py-3">
+                  <GlobeIcon className="h-5 w-5" />
+                </div>
+                <div className="flex items-center justify-center bg-gem-sapphire px-5 text-base font-semibold text-white">
+                  <Link to={"https://kiritoro.vercel.app"} target="_blank">
+                    Kiritoro Dev.
+                  </Link>
+                </div>
+              </div>
+
+              <div className="flex items-stretch justify-center">
+                <div className="flex items-center justify-center bg-white px-4 py-3">
+                  <GithubIcon className="h-5 w-5" />
+                </div>
+                <Link
+                  to={"https://github.com/kiritoroo"}
+                  target="_blank"
+                  className="flex items-center justify-center bg-gem-sapphire px-5 text-base font-semibold text-white"
+                >
+                  @kiritoroo
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-stretch justify-center">
+              <div className="flex items-center justify-center bg-white px-4 py-3">
+                <MailIcon className="h-5 w-5" />
+              </div>
+              <div className="flex items-center justify-center bg-gem-sapphire px-5 text-base font-semibold text-white">
+                <Link to={"mailto:trungle30902@gmail.com"} target="_blank">
+                  trungle30902@gmail.com
+                </Link>
+              </div>
+            </div>
+
+            <div className="pt-3 text-base font-medium">Ho Chi Minh City, Viet Nam</div>
           </div>
 
           <div className="absolute right-[30px] top-[30px]">
